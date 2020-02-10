d988321f43d73702ad7ac8a4a237dbe73f72bc6a
Kaeyota <littledragonchild@gmail.com>
2020-02-09 22:58:25 -0700

    Lightning tribulation test

--- src/main/java/io/github/Kaeyota/CultivatorMod/LoginHandler.java
+++ src/main/java/io/github/Kaeyota/CultivatorMod/LoginHandler.java
@@ -6,19 +6,29 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerExpChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLevelChangeEvent;
 
 public class LoginHandler implements Listener {
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event)
-    {
+    public void onPlayerJoin(PlayerJoinEvent event) {
         Player p = event.getPlayer();
-        event.setJoinMessage(p.getDisplayName() + ChatColor.YELLOW + " has joined!");
+        event.setJoinMessage(p.getDisplayName() + ChatColor.BLUE + " has joined!");
     }
 
     @EventHandler
     public void onPlayerExpChange(PlayerExpChangeEvent event) {
         Player p = event.getPlayer();
-        p.sendMessage("You got " + event.getAmount() + " exp");
+        // TODO: Implement bottle necks
+    }
 
+    @EventHandler
+    public void onLevelChange(PlayerLevelChangeEvent event) {
+        if (event.getNewLevel() - event.getOldLevel() < 0)
+            return;
+        if (event.getNewLevel() >= 30) {
+            Player player = event.getPlayer();
+            player.sendMessage("Welcome to the new world of Cultivation. Don't die.");
+            player.getWorld().strikeLightning(player.getLocation());
+        }
     }
 }
