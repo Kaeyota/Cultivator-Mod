5c6c479918699ab1700e34dae854eebaf0a49e91
Michael Clark <clark.michael.c@gmail.com>
2020-03-08 21:33:11 -0600

    Refactor - order bottom up

--- src/main/java/io/github/Kaeyota/CultivatorMod/Tribulation.java
+++ src/main/java/io/github/Kaeyota/CultivatorMod/Tribulation.java
@@ -44,6 +44,56 @@ public class Tribulation implements Listener {
         }*/
     }
 
+    // TODO - doesn't accurately represent damage, ignores a new armor type, protection, etc.
+    // Can we hook into the real calculations?
+    // Calculate the percent damage reduction from armor
+    public double getDamageReduced(Player player)
+    {
+        PlayerInventory inv = player.getInventory();
+        ItemStack boots = inv.getBoots();
+        ItemStack helmet = inv.getHelmet();
+        ItemStack chest = inv.getChestplate();
+        ItemStack pants = inv.getLeggings();
+        double red = 0.0;
+        if (helmet != null) {
+            if (helmet.getType() == Material.LEATHER_HELMET) red = red + 0.04;
+            else if (helmet.getType() == Material.GOLDEN_HELMET) red = red + 0.08;
+            else if (helmet.getType() == Material.CHAINMAIL_HELMET) red = red + 0.08;
+            else if (helmet.getType() == Material.IRON_HELMET) red = red + 0.08;
+            else if (helmet.getType() == Material.DIAMOND_HELMET) red = red + 0.12;
+        }
+        //
+        if (boots != null) {
+            if (boots.getType() == Material.LEATHER_BOOTS) red = red + 0.04;
+            else if (boots.getType() == Material.GOLDEN_BOOTS) red = red + 0.04;
+            else if (boots.getType() == Material.CHAINMAIL_BOOTS) red = red + 0.04;
+            else if (boots.getType() == Material.IRON_BOOTS) red = red + 0.08;
+            else if (boots.getType() == Material.DIAMOND_BOOTS) red = red + 0.12;
+        }
+        //
+        if (pants != null) {
+            if (pants.getType() == Material.LEATHER_LEGGINGS) red = red + 0.08;
+            else if (pants.getType() == Material.GOLDEN_LEGGINGS) red = red + 0.12;
+            else if (pants.getType() == Material.CHAINMAIL_LEGGINGS) red = red + 0.16;
+            else if (pants.getType() == Material.IRON_LEGGINGS) red = red + 0.20;
+            else if (pants.getType() == Material.DIAMOND_LEGGINGS) red = red + 0.24;
+        }
+        //
+        if (chest != null) {
+            if (chest.getType() == Material.LEATHER_CHESTPLATE) red = red + 0.12;
+            else if (chest.getType() == Material.GOLDEN_CHESTPLATE) red = red + 0.20;
+            else if (chest.getType() == Material.CHAINMAIL_CHESTPLATE) red = red + 0.20;
+            else if (chest.getType() == Material.IRON_CHESTPLATE) red = red + 0.24;
+            else if (chest.getType() == Material.DIAMOND_CHESTPLATE) red = red + 0.32;
+        }
+        return red;
+    }
+
+    protected void tribulationStrike(final Player player, final double damage) {
+        Entity tribulation = player.getWorld().strikeLightningEffect(player.getLocation());
+        player.damage(damage*(1-getDamageReduced(player)), tribulation);
+    }
+
     @EventHandler
     public void onLevelChange(PlayerLevelChangeEvent event) {
         // Do nothing if losing levels
@@ -112,53 +162,5 @@ public class Tribulation implements Listener {
                 player.playSound(player.getLocation(), Sound.BLOCK_SLIME_BLOCK_BREAK, 1.0F, 1.0F);
                 break;
         }
-    }
-
-    protected void tribulationStrike(Player player, double damage) {
-        Entity tribulation = player.getWorld().strikeLightningEffect(player.getLocation());
-        player.damage(damage*(1-getDamageReduced(player)), tribulation);
-    }
-
-    // Calculate the percent damage reduction from armor
-    public double getDamageReduced(Player player)
-    {
-        PlayerInventory inv = player.getInventory();
-        ItemStack boots = inv.getBoots();
-        ItemStack helmet = inv.getHelmet();
-        ItemStack chest = inv.getChestplate();
-        ItemStack pants = inv.getLeggings();
-        double red = 0.0;
-        if (helmet != null) {
-            if (helmet.getType() == Material.LEATHER_HELMET) red = red + 0.04;
-            else if (helmet.getType() == Material.GOLDEN_HELMET) red = red + 0.08;
-            else if (helmet.getType() == Material.CHAINMAIL_HELMET) red = red + 0.08;
-            else if (helmet.getType() == Material.IRON_HELMET) red = red + 0.08;
-            else if (helmet.getType() == Material.DIAMOND_HELMET) red = red + 0.12;
-        }
-        //
-        if (boots != null) {
-            if (boots.getType() == Material.LEATHER_BOOTS) red = red + 0.04;
-            else if (boots.getType() == Material.GOLDEN_BOOTS) red = red + 0.04;
-            else if (boots.getType() == Material.CHAINMAIL_BOOTS) red = red + 0.04;
-            else if (boots.getType() == Material.IRON_BOOTS) red = red + 0.08;
-            else if (boots.getType() == Material.DIAMOND_BOOTS) red = red + 0.12;
-        }
-        //
-        if (pants != null) {
-            if (pants.getType() == Material.LEATHER_LEGGINGS) red = red + 0.08;
-            else if (pants.getType() == Material.GOLDEN_LEGGINGS) red = red + 0.12;
-            else if (pants.getType() == Material.CHAINMAIL_LEGGINGS) red = red + 0.16;
-            else if (pants.getType() == Material.IRON_LEGGINGS) red = red + 0.20;
-            else if (pants.getType() == Material.DIAMOND_LEGGINGS) red = red + 0.24;
-        }
-        //
-        if (chest != null) {
-            if (chest.getType() == Material.LEATHER_CHESTPLATE) red = red + 0.12;
-            else if (chest.getType() == Material.GOLDEN_CHESTPLATE) red = red + 0.20;
-            else if (chest.getType() == Material.CHAINMAIL_CHESTPLATE) red = red + 0.20;
-            else if (chest.getType() == Material.IRON_CHESTPLATE) red = red + 0.24;
-            else if (chest.getType() == Material.DIAMOND_CHESTPLATE) red = red + 0.32;
-        }
-        return red;
     }
 }
